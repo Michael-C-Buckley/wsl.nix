e9686d54b87bd18dda27f1169351bb45c285627e
Michael Buckley <michaelcbuckley@proton.me>
2025-03-25 13:58:38 -0400

    Removed Maple since it fails to build currently

--- pkgs/fonts.nix
+++ pkgs/fonts.nix
@@ -7,13 +7,12 @@
     fira-code
     jetbrains-mono
     symbols-only
-    iosevka
     zed-mono
   ];
 in {
   fonts.packages = with pkgs; lib.optionals useFonts [
     vista-fonts
-    maple-mono
+    # maple-mono
     b612
     font-awesome
   ] ++ nerdFonts;
